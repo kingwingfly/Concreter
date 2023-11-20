c3b395853e2c205352eb3cb0390d965b80c14865
Louis <836250617@qq.com>
2023-11-21 05:54:57 +0800

    py rpc quick start

--- src_py/main.py
+++ src_py/main.py
@@ -1,7 +1,19 @@
-def foo():
-    a = 1 +1
-    return a
+from pb import sym_pb2_grpc
+from pb.sym_pb2_grpc import SymServicer
+from pb.sym_pb2 import HelloReply
+import grpc
+
+from concurrent import futures
+
+
+class SymServer(SymServicer):
+    def SayHello(self, request, context):
+        return HelloReply(message=f"Hello, {request.name}!")
 
 
 if __name__ == "__main__":
-    print(f"1 + 1 = {1 + 1}")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    sym_pb2_grpc.add_SymServicer_to_server(SymServicer(), server)
+    server.add_insecure_port("[::]:50051")
+    server.start()
+    server.wait_for_termination()
